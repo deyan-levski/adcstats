00e734a326ea0a710279690fc23e20ff39f44d86
Deyan Levski <didolevski@gmail.com>
2017-01-19 13:25:06 +0200

    Modified stats script

--- analyzePGM.m
+++ analyzePGM.m
@@ -2,22 +2,56 @@ clc;
 clear all;
 close all;
 
-pgmFile = 'snapshot000.pgm';
-analyzeColumn = 1024;
-columnsTotal = 1024; %1024
+pgmFile = 'snapshots/snapshot000.pgm';
+pgmFile1 = 'snapshots/snapshot001.pgm';
+pgmFile2 = 'snapshots/snapshot002.pgm';
+pgmFile3 = 'snapshots/snapshot003.pgm';
+pgmFile4 = 'snapshots/snapshot004.pgm';
+pgmFile5 = 'snapshots/snapshot005.pgm';
+pgmFile6 = 'snapshots/snapshot006.pgm';
+pgmFile7 = 'snapshots/snapshot007.pgm';
+pgmFile8 = 'snapshots/snapshot008.pgm';
+pgmFile9 = 'snapshots/snapshot009.pgm';
+pgmFile10 = 'snapshots/snapshot010.pgm';
+pgmFile11 = 'snapshots/snapshot011.pgm';
+
+analyzeColumn = 68;
+columnsTotal = 128; %1024
 artOffset = 0;
 
 imageIn = double(imread(pgmFile)/16); % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile1)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile2)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile3)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile4)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile5)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile6)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile7)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile8)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile9)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile10)/16)]; % div by 16 to scale 16bit to 12bit
+imageIn = [imageIn; double(imread(pgmFile11)/16)]; % div by 16 to scale 16bit to 12bit
+
 
 imageIn = imageIn(:,1:columnsTotal);
 
-
 % Histograms
 
-column = imageIn(:,analyzeColumn);
-% 2D
+% Total array histogram
 figure();
-hist(column+artOffset);
+
+histogram(imageIn);
+xlabel('Code [LSB]');
+ylabel('Density [N]');
+title('Code Density Histogram');
+
+
+% 2D
+
+column = imageIn(:,analyzeColumn);
+
+figure();
+histogram(column+artOffset);
 
 meanColumn = mean(column);
 stdColumn = std(column);
