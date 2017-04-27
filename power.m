634fa8f1b5279be572c5d9a819244fbc359fa392
Deyan <didolevski@gmail.com>
2017-04-27 14:55:47 +0100

    Added power charts / updated

--- power.m
+++ power.m
@@ -2,6 +2,6 @@ total = 177;
 
 POWER = [8 30 68 30 31];
 
-LABELS = {'Digital References', 'Digital Columns + SRAM', 'Analog Columns', 'Analog References', 'Serial + LVDS'};
+LABELS = {'DVDD References — 8.85 mW', 'DVDD Columns (TDC+SS) — 31.8 mW', 'AVDD Columns (S/H + COMP) — 72.5 mW', 'AVDD References — 31.8 mW', 'DVDD LVDS — 33.6 mW'};
 
 pie(POWER,LABELS);
