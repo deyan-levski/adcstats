0039240691db703aa861e7e54e99dc723572ab25
Deyan <didolevski@gmail.com>
2017-05-04 12:27:50 +0100

    Adding power3d pie fill

--- power.m
+++ power.m
@@ -1,7 +1,8 @@
 total = 177;
 
-POWER = [8 30 68 30 31];
+POWER = [8 30 98 30 31];
 
-LABELS = {'DVDD References — 8.85 mW', 'DVDD Columns (TDC+SS) — 31.8 mW', 'AVDD Columns (S/H + COMP) — 72.5 mW', 'AVDD References — 31.8 mW', 'DVDD LVDS — 33.6 mW'};
+LABELS = {'DVDD References — 8.85 mW', 'DVDD Columns (TDC+SS) — 31.8 mW', 'AVDD Columns (S/H + COMP) — 102.5 mW', 'AVDD References — 31.8 mW', 'DVDD LVDS — 33.6 mW'};
 
-pie(POWER,LABELS);
+h=pie3(POWER,LABELS);
+set(h(2:2:6),'FontSize',14);
